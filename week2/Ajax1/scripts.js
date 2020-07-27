471fbdbe81c19ede70bde681ffd4b542d6180eb7
vishnulinga <67555130+vishnulinga@users.noreply.github.com>
2020-07-27 22:13:14 +0530

    Add files via upload

---
+++ week2/Ajax1/scripts.js
@@ -0,0 +1,17 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+var url_string = 'https://dog.ceo/api/breeds/list/all';
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET',url_string , true)
+request.send();
+request.onload = function() {
+  // Begin accessing JSON data here
+var data = JSON.parse(this.response)
+console.log(data);
+}
+
+/*
+fetch(url_string) // Call the fetch function passing the url of the API as a parameter
+.then(res=>res.json())
+.then(data=>console.log(data))
+*/
