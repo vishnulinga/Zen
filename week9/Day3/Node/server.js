0459a9525890a80aefe712fac5a3c4d0cde63734
vishnulinga <vishnulinga2@gmail.com>
2020-09-19 15:37:50 +0530

    finalcommit

--- week9/Day3/Node/server.js
+++ week9/Day3/Node/server.js
@@ -1,19 +1,28 @@
 var studentcount=0,mentorcount=0
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const express=require("express");
 const app=express();
 const bodyparser=require("body-parser");
 app.use(bodyparser.json())
 const mongodb=require("mongodb");
 const mongoclient=mongodb.MongoClient;
-const url="mongodb://localhost:27017";
+const url="mongodb+srv://vishnu:123abc@cluster0.o2tjj.mongodb.net/taskdata?retryWrites=true&w=majority";
+const cors=require("cors")
+app.use(cors({
+    origin:"*"
+}))
 
-//create student and mentor.......................
+//create student and mentor........................js
+
 app.post("/student",async function(req,res){
-studentcount++;    
+
+    
 let client=await mongoclient.connect(url,{ useUnifiedTopology: true })
 let db=client.db("taskdata")
-await db.collection("students").insertOne({_id:studentcount,name:req.body.name,mentor:[]})
+let studentcount=await db.collection("studentcount").findOne({_id:"count"})
+console.log(studentcount, typeof(studentcount),studentcount["count"])
+await db.collection("students").insertOne({_id:studentcount["count"]+1,name:req.body.name,mentor:[]})
+await db.collection('studentcount').findOneAndUpdate({"_id":"count"},{$set:{"count":studentcount["count"]+1}})
 client.close()
 
 
@@ -31,12 +40,16 @@ app.get("/students",async function(req,res){
     let arr=await db.collection("students").find().toArray()
     client.close() 
     res.json(arr)
+
+    
 })
 app.post("/mentor",async function(req,res){
-    mentorcount++;    
+       
     let client=await mongoclient.connect(url,{ useUnifiedTopology: true })
     let db=client.db("taskdata")
-    await db.collection("mentors").insertOne({_id:mentorcount,name:req.body.name,studentslist:[]})
+    let mentorcount=await db.collection("mentorcount").findOne({_id:"count"})
+    await db.collection("mentors").insertOne({_id:mentorcount["count"]+1,name:req.body.name,studentslist:[]})
+    await db.collection('mentorcount').findOneAndUpdate({"_id":"count"},{$set:{"count":mentorcount["count"]+1}})
     client.close()
    
     // mentors.push(
@@ -59,10 +72,11 @@ app.get("/studentsof/:id",async function(req,res){
    try{
     let client=await mongoclient.connect(url,{ useUnifiedTopology: true })
     let db=client.db("taskdata")
-    let id=+req.params.id
-    let mentor=await db.mentors.find({"_id":id}).toArray(),arr=[]
-    mentor["studentslist"].forEach(id => {
-             arr.push(students[id])
+    let id=+req.params.id,arr=[]
+    let mentor=await db.collection('mentors').find({"_id":id}).toArray()
+    console.log(mentor)
+    mentor[0]["studentslist"].forEach(id => {
+             arr.push(id)
             })     
     res.json(arr)}catch(err){
         console.log(err);
@@ -75,7 +89,7 @@ app.post("/mentorofstudent/:s_id/:m_id",async function(req,res){
     let db=client.db("taskdata")
     let s_id=+req.params.s_id
     let m_id=+req.params.m_id
-    await db.students.findOneAndUpdate({"_id":s_id},{$set:{"mentor":[m_id]}})
+    await db.collection('students').findOneAndUpdate({"_id":s_id},{$set:{"mentor":[m_id]}})
     // students[req.params.s_id-1]["mentor"][0]=+req.params.m_id
    res.json({message:"mentor added to student"})}catch(err){
     console.log(err)   
@@ -85,10 +99,11 @@ app.post("/mentorofstudent/:s_id/:m_id",async function(req,res){
 app.post("/studentsofmentor/:m_id",async function(req,res){
     try{
     var m_id=+req.params.m_id
-    var list=req.body.arr
+    var list=req.body.arr.split(",").map(x=>+x)
+    console.log(typeof(list),list,m_id)
     let client=await mongoclient.connect(url,{ useUnifiedTopology: true })
     let db=client.db("taskdata"),arr=[]
-    db.mentors.findOneAndUpdate({"_id":m_id},{$set:{"studentslist":req.body.arr}})
+    db.collection('mentors').findOneAndUpdate({"_id":m_id},{$set:{"studentslist":list}})
     res.json({message:"students added"})}catch(err){
         console.log(err)
         res.json({"message":"error!"})}
