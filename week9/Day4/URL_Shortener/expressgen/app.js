f292b7c34e2cda0ab87c7cef76ac069033ed5069
vishnulinga <vishnulinga2@gmail.com>
2020-09-22 03:25:05 +0530

    urlshortener

--- week9/Day4/URL_Shortener/expressgen/app.js
+++ week9/Day4/URL_Shortener/expressgen/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors=require("cors")
+var nodemailer=require("nodemailer")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,10 +12,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 
-
+app.set("view engine","jade")
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
